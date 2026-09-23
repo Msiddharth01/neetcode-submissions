106ece9412240fee0320295272369eecbbcc062c
Siddharth Malik <msiddharth286@gmail.com>
2026-09-23 19:01:15 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int n = s.size();
+        int freq[26] = {0};
+        for(int i=0;i<n;i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return  true;
+    }
+};
